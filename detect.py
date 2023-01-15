3886da4fabc0ad3018e5ad115cefc38d78a51190
huiminlim <e0036379@u.nus.edu>
2023-01-15 11:13:23 +0800

    Add new dependencies of scikit learn

---
+++ detect.py
@@ -0,0 +1,6 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from skimage import transform
+from skimage.color import rgb2gray
+from skimage.feature import match_template, peak_local_max
+from skimage.io import imread, imshow
