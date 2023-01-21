7d611e6a6344f2395028cf33860e78bf120ca5ea
huiminlim <e0036379@u.nus.edu>
2023-01-22 01:34:45 +0800

    Add output x,y info

--- detect.py
+++ detect.py
@@ -11,34 +11,39 @@ from skimage.feature import match_template, peak_local_max
 from skimage.io import imread, imshow
 
 # Set directory path of current folder
-DATA_DIR = os.path.dirname(os.path.abspath(__file__)) + "\\data\\sample_2"
-actual_image = f"{DATA_DIR}\\PXL_20230118_110244696.MP.jpg"
+DATA_DIR = os.path.dirname(os.path.abspath(__file__)) + "\\data\\sample_1"
+actual_image = f"{DATA_DIR}\\PXL_20230118_110219929.MP.jpg"
 template_image = f"{DATA_DIR}\\template.jpg"
 mask_image = f"{DATA_DIR}\\mask.jpg"
 
 actual = cv2.imread(actual_image)
 mask = cv2.imread(mask_image, 0)
-actual = cv2.bitwise_and(actual, actual, mask=mask)
-# cv2.imshow("", before)
-# cv2.waitKey(1)
+template = imread(template_image)
 
-# before = imread(before_image)
+alpha = 1.2  # Contrast control (1.0-3.0)
+beta = 0  # Brightness control (0-100)
+actual = cv2.convertScaleAbs(actual, alpha=alpha, beta=beta)
+template = cv2.convertScaleAbs(template, alpha=alpha, beta=beta)
+actual = cv2.bitwise_and(actual, actual, mask=mask)
+template_grey = rgb2gray(template)
+
 actual_grey = rgb2gray(actual)
 imshow(actual_grey)
-
-template = imread(template_image)
-template_grey = rgb2gray(template)
 result = match_template(actual_grey, template_grey)
 
 template_width, template_height = template_grey.shape
-for x, y in peak_local_max(result, threshold_abs=0.5):
+for x, y in peak_local_max(result, threshold_abs=0.7):
     rect = plt.Rectangle((y, x),
-                         template_height,
-                         template_width,
+                         5,
+                         5,
                          color='r',
                          fc='none')
     plt.gca().add_patch(rect)
 
-output = f"{DATA_DIR}\\result.jpg"
+
+with open(f"{DATA_DIR}\\output.txt", "w") as f:
+    for x, y in peak_local_max(result, threshold_abs=0.7):
+        f.writelines(f"{x} {y}\n")
+
+
 plt.show()
-plt.savefig(output)
