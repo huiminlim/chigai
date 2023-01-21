2aa219bf4733ed907424e0a95f77d99532ae7afa
huiminlim <e0036379@u.nus.edu>
2023-01-21 14:14:17 +0800

    Add new sample data

--- detect.py
+++ detect.py
@@ -8,27 +8,27 @@ import matplotlib.pyplot as plt
 from matplotlib import pyplot as plt
 from skimage.color import rgb2gray
 from skimage.feature import match_template, peak_local_max
-from skimage.io import imread, imsave, imshow
+from skimage.io import imread, imshow
 
 # Set directory path of current folder
-DATA_DIR = os.path.dirname(os.path.abspath(__file__)) + "\\data\\toy"
-before_image = f"{DATA_DIR}\\after.jpg"
+DATA_DIR = os.path.dirname(os.path.abspath(__file__)) + "\\data\\sample_2"
+actual_image = f"{DATA_DIR}\\PXL_20230118_110244696.MP.jpg"
 template_image = f"{DATA_DIR}\\template.jpg"
 mask_image = f"{DATA_DIR}\\mask.jpg"
 
-before = cv2.imread(before_image)
-mask_before = cv2.imread(mask_image, 0)
-before = cv2.bitwise_and(before, before, mask = mask_before)
+actual = cv2.imread(actual_image)
+mask = cv2.imread(mask_image, 0)
+actual = cv2.bitwise_and(actual, actual, mask = mask)
 # cv2.imshow("", before)
 # cv2.waitKey(1)
 
 # before = imread(before_image)
-before_grey = rgb2gray(before)
-imshow(before_grey)
+actual_grey = rgb2gray(actual)
+imshow(actual_grey)
 
 template = imread(template_image)
 template_grey = rgb2gray(template)
-result = match_template(before_grey, template_grey)
+result = match_template(actual_grey, template_grey)
 
 template_width, template_height = template_grey.shape
 for x, y in peak_local_max(result, threshold_abs=0.5):
@@ -40,5 +40,5 @@ for x, y in peak_local_max(result, threshold_abs=0.5):
     plt.gca().add_patch(rect)
 
 output = f"{DATA_DIR}\\result.jpg"
-imsave(output,result)
 plt.show()
+plt.savefig(output)
